96d05a92eb0c9fabef64335a7f54ca15c01f3d1f
KvaskovSS <sava0980@yandex.ru>
2023-01-26 15:08:44 +0300

    Complete task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+void printArr(string[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i == arr.Length - 1 )
+        {
+            Console.Write($"{arr[i]}");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+    }
+}
+
+void fillArr(string[] arr)
+{
+    Console.Write("Заполните массив :");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Console.ReadLine();
+    }
+}
+
+void newArr(string[] arr)
+{
+    Console.WriteLine("Новый массив");
+
+    int j = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length < 3)
+        {
+            j++;
+        }
+    }
+
+    string[] newArrString = new string[j];
+    j = 0;
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length < 3)
+        {
+            newArrString[j] = arr[i];
+            j++;
+        }
+    }
+    printArr(newArrString);
+}
+
+Console.WriteLine("Введите длину массива");
+int length = Convert.ToInt32(Console.ReadLine());
+
+
+string[] arrString = new string[length];
+
+fillArr(arrString);
+printArr(arrString);
+newArr(arrString);
